1734ebbf419940141763233e139b65197661b7ef
zsq <zsq.471214943@163.com>
2017-09-09 16:43:02 +0800

    增加数据传输层

---
+++ jjly-service-common/src/main/java/com/jjly/vo/UserVO.java
@@ -0,0 +1,13 @@
+package com.jjly.vo;
+
+/**
+ * <p>用户vo类 </p>
+ *
+ * @author Steven
+ * @version V1.0
+ * @Package com.jjly.vo
+ * @e-mail 47121494@qq.com
+ * @date 2017/9/916:42
+ */
+public class UserVO {
+}
