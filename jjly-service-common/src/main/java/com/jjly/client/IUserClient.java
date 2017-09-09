c1bc42983402eb4fec3cce6113075e49dc5b089d
zsq <zsq.471214943@163.com>
2017-09-09 16:41:15 +0800

    增加Client的层级结构

---
+++ jjly-service-common/src/main/java/com/jjly/client/IUserClient.java
@@ -0,0 +1,13 @@
+package com.jjly.client;
+
+/**
+ * <p>用户逻辑接口</p>
+ *
+ * @author Steven
+ * @version V1.0
+ * @Package com.jjly.client
+ * @e-mail 47121494@qq.com
+ * @date 2017/9/916:39
+ */
+public interface IUserClient {
+}
---
+++ jjly-service-common/src/main/java/com/jjly/client/impl/UserClientImpl.java
@@ -0,0 +1,15 @@
+package com.jjly.client.impl;
+
+import com.jjly.client.IUserClient;
+
+/**
+ * <p>用户业务逻辑类 </p>
+ *
+ * @author Steven
+ * @version V1.0
+ * @Package com.jjly.client.impl
+ * @e-mail 47121494@qq.com
+ * @date 2017/9/916:37
+ */
+public class UserClientImpl implements IUserClient{
+}
